3d83a449742162608f435d96fb7322b6c431f41b
kouichiUme <kouichiume@gmail.com>
2020-05-05 23:43:07 +0900

    ui file

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { createStackNavigator } from 'react-navigation'
 export default function App() {
+
+
+  let programName = ""
+
+  createStackNavigator()
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home Robot Arm Powered by ARSC </Text>
+
+      <Text>Information : </Text>
+      <Text>Program: </Text>
+      <TextInput placeholder="Program Name" value={programName} onChangeText={changeProgramName} />
+      <Button title="Load Program" onPress={loadProgramClicked} />
+      <Button title="run program" ></Button>
+      <Button title="FWD" ></Button>
+      <Button title="REV" ></Button>
+      <Button title="STOP" ></Button>
+
+      <Text>  Infromation now this is mock</Text>
+      <Text >Current Row</Text>
     </View>
   );
 }
@@ -17,3 +36,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function loadProgramClicked() {
+  return "cliecked"
+}
+
+
+function changeProgramName() {
+  return ""
+}
+
